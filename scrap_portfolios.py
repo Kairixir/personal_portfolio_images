d4b998095fac6f0ab57ab49e394a34faff86fc4f
Kairixir <kairixir@protonmail.com>
2024-05-04 16:20:33 +0200

    fix: replace slashes with dashes in website name

--- scrap_portfolios.py
+++ scrap_portfolios.py
@@ -12,7 +12,7 @@ img_tags = [td.find("a") for td in soup.find_all("table")[1].find_all("td")[1::5
 for web_link, img_tag in zip(website_links, img_tags):
     r = requests.get(img_tag["href"], stream = True)
     if r.status_code == 200:
-        with open(web_link.text + ".png", 'wb') as f:
+        with open(re.sub("/", "-", web_link.text) + ".png", 'wb') as f:
             r.raw.decode_content = True
             shutil.copyfileobj(r.raw, f)
     else:
